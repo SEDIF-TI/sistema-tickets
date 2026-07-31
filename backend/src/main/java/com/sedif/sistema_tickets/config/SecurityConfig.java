00c3bf8bbeb727d9f1a06daa0abf029a17229742
Emmanuel <emanuel;pajrito@gmail.com>
2026-07-30 18:32:50 -0600

    feat: dockerización completa del sistema (frontend, backend, bd) y configuración de CORS

--- backend/src/main/java/com/sedif/sistema_tickets/config/SecurityConfig.java
+++ backend/src/main/java/com/sedif/sistema_tickets/config/SecurityConfig.java
@@ -69,9 +69,8 @@ public class SecurityConfig {
         
         config.setAllowCredentials(true);
         
-        // 1. CAMBIO CLAVE: Ponemos tu puerto exacto de React en lugar del asterisco (*)
-        // Agregamos también el 5174 por si Vite llega a saltar de puerto en algún momento
-        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174", "http://localhost:4173")); 
+        
+        config.setAllowedOrigins(List.of("http://localhost", "http://localhost:5173", "http://localhost:5174", "http://localhost:4173")); 
         
         config.setAllowedHeaders(List.of("*"));
         
